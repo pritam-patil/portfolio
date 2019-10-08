ce4c676f9aad972cc541950809eb1c70d7468bbb
pritam-patil <pritam18121990@gmail.com>
2019-10-08 21:59:05 +0530

    docs for deploy

--- docs/precache-manifest.3a693122d46b93c02b9f6a5a26a618bf.js
+++ docs/precache-manifest.3a693122d46b93c02b9f6a5a26a618bf.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "94ce2e0a4699153550dd",
-    "url": "/portfolio/static/js/main.94ce2e0a.chunk.js"
+    "revision": "e7c5621a5bf250f3a96a",
+    "url": "/portfolio/static/js/main.e7c5621a.chunk.js"
   },
   {
     "revision": "e8578e0d7e89955f6cdb",
     "url": "/portfolio/static/js/1.e8578e0d.chunk.js"
   },
   {
-    "revision": "94ce2e0a4699153550dd",
+    "revision": "e7c5621a5bf250f3a96a",
     "url": "/portfolio/static/css/main.a2ce0c50.chunk.css"
   },
   {
-    "revision": "46a2ddb01655f918536522e7e0e24495",
+    "revision": "90bedc8487687e0d74bc7c55891eb5a8",
     "url": "/portfolio/index.html"
   }
 ];
